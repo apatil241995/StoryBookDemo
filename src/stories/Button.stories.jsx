e15a19ae74927d82ecfbd134a2988a0773bf349c
apatil241995 <patilanish39@gmail.com>
2022-12-06 13:24:18 +0530

    First Commit

---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { Button } from '../components/Buttons/Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Primary Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Secondary Button',
+  color: '#333',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Large Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Small Button',
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  size: 'medium',
+  label: 'Medium Button',
+};
